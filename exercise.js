7bc9988864cb32ac89bd1d45b996cb36a9c929af
MarcelVoigt92 <Marcel.Voigt92@gmx.de>
2022-09-27 14:22:03 +0200

    first commit

---
+++ exercise.js
@@ -0,0 +1,38 @@
+// Exercise1
+let numOne = 12;
+let numTwo = 20;
+
+console.log(numOne + numTwo);
+// Exercise2
+const X = 12;
+
+console.log(X);
+// Exercise3
+const nameOne = "John Doe";
+
+console.log(nameOne);
+
+// Exercise4
+console.log(numOne - X);
+
+// Exercise5
+console.log(X + nameOne);
+
+// Exercise6
+const hallo = "Was geht";
+const nummer = 42;
+const bobo = true;
+let yoyo;
+const age = null;
+
+console.log(typeof hallo);
+console.log(typeof nummer);
+console.log(typeof bobo);
+console.log(typeof yoyo);
+console.log(typeof null);
+
+// Exercise7
+const numberOne = 20;
+const numberTwo = 4;
+
+console.log(numberOne % numberTwo);
